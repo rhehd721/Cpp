93ae70ff401e998bc5694a0e3c0aa634398b9660
알파카 <rhehd721@naver.com>
2021-03-29 13:41:00 +0900

    프로그래머스 알고리즘

--- Algorithm/Programmers/Sort_LargestNum/LargestNum.cpp
+++ Algorithm/Programmers/Sort_LargestNum/LargestNum.cpp
@@ -22,6 +22,8 @@ string solution(vector<int> numbers) {
     vector<int> b;
     vector<int> c;
     vector<int> d;
+    vector<string> num1;
+    vector<string> num2;
 
     sort(numbers.begin(), numbers.end(), greater<int>());
 
@@ -47,31 +49,54 @@ string solution(vector<int> numbers) {
     while(1){
         for(int i = 9; i > 0; i --){
             int i_num = 0;  // 갯수
+            int ii_num = 0;  // 갯수
+            int iii_num = 0;  // 갯수
             string s_num = "";   // 해당 숫자
             string ss_num = "";
             string sss_num = "";
 
-            // 9 를 찾아라
+            // 9 를 찾아라      // 3(3) 34(1) 335(2) 321(4)
             s_num = s_num + to_string(i);
             i_num = count(a.begin(), a.end(), stoi(s_num));
-            for (int z = 0; z < i_num; z++){
-                answer += s_num;
-            }
+            // for (int z = 0; z < i_num; z++){
+            //     answer += s_num;
+            // }
+
             // 99 ~ 90 을 찾아라
             for(int j = 9; j >= 0; j --){
+                
                 ss_num = s_num + to_string(j);
-                i_num = count(b.begin(), b.end(), stoi(ss_num));
-                for (int z = 0; z < i_num; z++){
-                    answer += ss_num;
-                }
+                ii_num = count(b.begin(), b.end(), stoi(ss_num));
+                
                 // 999 ~ 990 을 찾아라
-                for(int k = 9; k >= 0; k --)
+                for(int k = 9; k >= 0; k --){
+                    if(j > i && k > j){}
                     sss_num = ss_num + to_string(k);
-                    i_num = count(c.begin(), c.end(), stoi(sss_num));
-                    for (int z = 0; z < i_num; z++){
-                        answer += sss_num;
+                    iii_num = count(c.begin(), c.end(), stoi(sss_num));
+                    
+                    if (j >= i && k >= j){  // 미리 넣기 ex. 333, 334, 345, 344
+                        for (int z = 0; z < iii_num; z++){
+                            answer += sss_num;
+                            iii_num = 0;
+                        }
+                    }
+                    else{   // 311, 331
+                        for (int z = 0; z < iii_num; z++){
+                            num2.push_back(sss_num);
+                        }
+                    }
                 }
+                if(j >= i){  // 미리 넣기 ex. 34, 33
+                    for (int z = 0; z < ii_num; z++){
+                        answer += ss_num;
+                    }
+                }
+                else{
+
+                }
+                
             }
+
         }
 
         for (int z = 0; z < d.size(); z++){
