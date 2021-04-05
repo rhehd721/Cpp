c59953cc053e587325088ed3af46e69c99f71d65
알파카 <rhehd721@naver.com>
2021-04-05 20:18:36 +0900

    kakao 문제 풀이

--- Algorithm/Programmers/Kakao_NewID/NewID.cpp
+++ Algorithm/Programmers/Kakao_NewID/NewID.cpp
@@ -26,7 +26,7 @@ using namespace std;
 string solution(string new_id) {
     string answer = "";
 
-    cout << "처음 : " << new_id << endl;
+    // cout << "처음 : " << new_id << endl;
 
     for (int i(0); i < new_id.size(); i++){
         // 1단계 대문자 -> 소문자
@@ -34,7 +34,7 @@ string solution(string new_id) {
             new_id[i] = char(int(new_id[i]) + 32);
         }
     }
-    cout << endl << "대문자 제거 : " << new_id << endl;
+    // cout << endl << "대문자 제거 : " << new_id << endl;
 
     // 2단계 소문자, 숫자, 빼기(-), 밑줄(_), 마침표(.) 이 아니라면 제거
     // 97 ~ 122 = 소문자
@@ -47,7 +47,7 @@ string solution(string new_id) {
             i--;
         }
     }
-    cout << endl << "필요없는 문자 제거 : " << new_id << endl;
+    // cout << endl << "필요없는 문자 제거 : " << new_id << endl;
 
     int start(0);
     int cnt(0);
@@ -59,39 +59,81 @@ string solution(string new_id) {
             cnt++;  // 마침표 갯수 증가
         }
         else{
-            if(!cnt){   // cnt가 존재한다면 작업
-                new_id.erase(new_id.begin() + start, new_id.begin() + start + cnt - 1);
-                start = 0;
-            }
-            else{   // cnt = 0 이면 아무런 동작 없음
+            if(!cnt){  // cnt = 0 이면 아무런 동작 없음 
                 continue;
+            }
+            else{   // cnt가 존재한다면 작업
+                // cout << endl << "before : " << new_id << endl;
+                new_id.erase(new_id.begin() + start, new_id.begin() + start + cnt - 1);
+                // cout << endl << "after : " << new_id << endl;
+                start = 0;
+                i -= cnt;
+                cnt = 0;
             }
         }
     }
-    cout << endl << "마침표 줄이기 : " << new_id << endl;
+    // cout << endl << "마침표 줄이기 : " << new_id << endl;
 
-    // 3단계 연속된 .는 하나로 통일
+    // 4단계 new_id에서 마침표(.)가 처음이나 끝에 위치한다면 제거합니다.
+    while(1){
+        if(new_id[0] == 46){
+            new_id.erase(new_id.begin());
+        }
+        else{
+            break;
+        }
+    }
+    while(1){
+        if(new_id[new_id.size() - 1] == 46){
+            new_id.erase(new_id.begin() + new_id.size() - 1);
+        }
+        else{
+            break;
+        }
+    }
+    // cout << endl << "맨처음 또는 마지막 마침표 재거 : " << new_id << endl;
+
+    // 5단계 new_id가 빈 문자열이라면, new_id에 "a"를 대입합니다.
+    if(new_id.size() == 0){
+        answer = "aaa";
+        // cout << endl << "빈문자라면 : " << new_id << endl;
+        return answer;
+    }
+
+    // 6단계 사이즈 줄이기
+    if(new_id.size() >= 16){
+        new_id.erase(new_id.begin() + 15, new_id.end());
+    }
+    // cout << endl << "15글자 이내 : " << new_id << endl;
+    while(1){
+        if(new_id[new_id.size() - 1] == 46){
+            new_id.erase(new_id.begin() + new_id.size() - 1);
+        }
+        else{
+            break;
+        }
+    }
+
+    // 7단계
+    char x;
+    if(new_id.size() <= 2){
+        x = new_id[new_id.size() - 1];
+        for(int i = new_id.size(); i < 3; i++){
+            new_id += x;
+        }
+    }
 
 
-    // for (int i(0); i < new_id.size(); i++){
-    //     if(!((new_id[i] >= 48  && new_id[i] <= 57) || new_id[i] == 45 || new_id[i] == 46 || new_id[i] == 95 || (97 <= new_id[i] && new_id[i] <= 122))){
-    //         cout << new_id[i] << ", ";
-    //     }
-    // }
-    // cout << endl << new_id << endl;
-
-
-
-
+    answer = new_id;
 
     cout << answer << endl << endl;
     return answer;
 }
 int main(){
-    string new_id = "...!@BaT#*..y.abcdefghijklm";
-    solution(new_id); //	"bat.y.abcdefghi"
-    // new_id = "z-+.^.";
-    // solution(new_id); // "z--"
+    string new_id = "...!@BaT#*..y.abcdefghijklm.";
+    // solution(new_id); //	"bat.y.abcdefghi"
+    new_id = "z-+.^.";
+    solution(new_id); // "z--"
     // new_id = "=.=";
     // solution(new_id); // "aaa"
     // new_id = "123_.def";
