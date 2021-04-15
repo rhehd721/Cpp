ab2671d2656eabe968373a3105043aef4066e693
알파카 <rhehd721@naver.com>
2021-04-15 22:05:09 +0900

    프로그래머스 알고리즘

--- ProgrammersChallenge/test2.cpp
+++ ProgrammersChallenge/test2.cpp
@@ -9,9 +9,9 @@
 using namespace std;
 
 int solution(string s) {
-    int answer = -1;
+    int answer = 0;
 
-    if(!(s.size() % 2)){
+    if((s.size() % 2)){
         cout << 0 << endl;
         return 0;
     }
@@ -29,25 +29,52 @@ int solution(string s) {
     char C = '{';
     char c = '}';
 
-    int check_A(0);
-    int check_B(0);
-    int check_C(0);
-
     int idx(cycle);
 
     for (int i(0); i <= cycle; i++){
         int h(cycle - i);
-        cout << "cycle" << endl;
+        int check(0);
+        int check_A(0);
+        int check_B(0);
+        int check_C(0);
+        
         for(int k(0); k < s.size(); k++){
             if(h < 0){
                 h = cycle;
             }
-            cout << h << endl;
+            
+            if(s[h] == a){
+                check_A++;
+            }
+            else if(s[h] == b){
+                check_B++;
+            }
+            else if(s[h] == c){
+                check_C++;
+            }
+            else if(s[h] == A){
+                check_A--;
+            }
+            else if(s[h] == B){
+                check_B--;
+            }
+            else if(s[h] == C){
+                check_C--;
+            }
+
+            if(check_A < 0 || check_B < 0 || check_C < 0){
+                check = 1;
+                break;
+            }
+            // cout << h << endl;
             h--;
+        }
+        if(!check){
+            answer++;
         }
     }
 
-    // cout << answer << endl;
+    cout << answer << endl;
     return answer;
 }
 
