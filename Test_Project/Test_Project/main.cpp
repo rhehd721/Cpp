db59004b8ce1d29c1dbe711b6313b20d160278e5
PC323\Admin <rhehd721@likelion.org>
2021-05-19 16:20:26 +0900

    C  공부

--- Test_Project/Test_Project/main.cpp
+++ Test_Project/Test_Project/main.cpp
@@ -2,41 +2,30 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-// prices_len은 배열 prices의 길이입니다.
-int* solution(int prices[], size_t prices_len) {
+// 파라미터로 주어지는 문자열은 const로 주어집니다. 변경하려면 문자열을 복사해서 사용하세요.
+char* solution(const char* number, int k) {
     // return 값은 malloc 등 동적 할당을 사용해주세요. 할당 길이는 상황에 맞게 변경해주세요.
-    int* answer = (int*)malloc(sizeof(int)*prices_len);
-
-    for (int idx = 0; idx < prices_len; idx++) {
-        int tem = 0;
-        for (int idx2 = idx; idx2 < prices_len; idx2++) {
-            if (idx2 == idx) { continue; }
-
-            tem++;
-            if (prices[idx] > prices[idx2]) {   // 가격이 하락했다면
-                break;
-            }
-        }
-        answer[idx] = tem;
-    }
-
-    for (int idx = 0; idx < prices_len; idx++) {
-        printf("%d ,", answer[idx]);
-    }
+    char* answer = (char*)malloc(1);
     return answer;
 }
 
 int main() {
-    int prices[5];
-    prices[0] = 1;
-    prices[1] = 2;
-    prices[2] = 3;
-    prices[3] = 2;
-    prices[4] = 3;
 
-    // printf("%d", sizeof(prices) / sizeof(int));
-    
-    free(solution(prices, sizeof(prices) / sizeof(int) )); // [4, 3, 1, 1, 0]
+    const char* number;
+    int k;
+
+    number = "1924";
+    k = 2;
+    solution(number, k); // return "94"
+
+    number = "1231234";
+    k = 3;
+    solution(number, k); // return "3234"
+
+    number = "4177252841";
+    k = 4;
+    solution(number, k); // return "775841"
+
 
     return 0;
 }
