a4f24268c7981beb22f1851efb31a9fbb3c3df89
알파카 <rhehd721@naver.com>
2021-05-31 19:28:46 +0900

    c 공부

--- C/main.c
+++ C/main.c
@@ -1,7 +1,15 @@
 #include<stdio.h>
+#include<stdlib.h>
 
 int main() {
 	printf("Hello World! \n");
+
+	int a;
+	// scanf("문자를 입려?? : %d \n", &a);
+	scanf("%d", &a);
+
+	printf("입력한 숫자는 %d 입니다. \n", a);
+
 
 	return 0;
 }
